0b81ae9006f5bb677f16d496ac160c290324dbe9
VIKAS VAKIL VISHWAKARMA <vishwakarmavikas0102@gmail.com>
2025-01-03 15:06:53 +0000

    set up sentry for error tracking

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,68 @@
-export default {
-  reactStrictMode: true,
-  env: {
-    API_URL: "http://localhost:3000",
-  },
-  async headers() {
-    return [
+import {withSentryConfig} from '@sentry/nextjs';
+export default withSentryConfig({
+reactStrictMode: true,
+
+env: {
+API_URL: "http://localhost:3000",
+},
+
+async headers() {
+return [
+  {
+    source: '/(.*)',
+    headers: [
       {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'http://localhost:3000, https://cautious-waffle-pjg7prvjqxjg2x74-3000.app.github.dev', // Allow specific origins
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-Requested-With, Content-Type, Authorization',
-          },
-        ],
+        key: 'Access-Control-Allow-Origin',
+        value: 'http://localhost:3000, https://cautious-waffle-pjg7prvjqxjg2x74-3000.app.github.dev', // Allow specific origins
       },
-    ];
+      {
+        key: 'Access-Control-Allow-Methods',
+        value: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      },
+      {
+        key: 'Access-Control-Allow-Headers',
+        value: 'X-Requested-With, Content-Type, Authorization',
+      },
+    ],
   },
-};
+];
+}
+}, {
+// For all available options, see:
+// https://github.com/getsentry/sentry-webpack-plugin#options
+
+org: "student-z90",
+project: "javascript-nextjs",
+
+// Only print logs for uploading source maps in CI
+silent: !process.env.CI,
+
+// For all available options, see:
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+// Upload a larger set of source maps for prettier stack traces (increases build time)
+widenClientFileUpload: true,
+
+// Automatically annotate React components to show their full name in breadcrumbs and session replay
+reactComponentAnnotation: {
+enabled: true,
+},
+
+// Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+// This can increase your server load as well as your hosting bill.
+// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+// side errors will fail.
+// tunnelRoute: "/monitoring",
+
+// Hides source maps from generated client bundles
+hideSourceMaps: true,
+
+// Automatically tree-shake Sentry logger statements to reduce bundle size
+disableLogger: true,
+
+// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+// See the following for more information:
+// https://docs.sentry.io/product/crons/
+// https://vercel.com/docs/cron-jobs
+automaticVercelMonitors: true,
+});
